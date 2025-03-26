543aad8b3571c9b991ac84c8ab708218c8fcf8d2
Zain Ghosheh <zainghosheh1@ku.edu>
2025-03-26 12:27:46 -0500

    Initial Commit

---
+++ GymLog/GymLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  GymLogApp.swift
+//  GymLog
+//
+//  Created by Zain Ghosheh on 3/26/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GymLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
